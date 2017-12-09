c70653954a7910ff791bba7f03d51120d9b31a3a
AlexandreBrown <brownthedev@gmail.com>
2017-12-08 22:54:53 -0500

    creation fragment accueil

--- app/src/main/java/cstj/qc/ca/andromia/MainActivity.kt
+++ app/src/main/java/cstj/qc/ca/andromia/MainActivity.kt
@@ -1,7 +1,8 @@
 package cstj.qc.ca.andromia
 
+import android.app.Fragment
+import android.net.Uri
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
@@ -9,10 +10,15 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.WindowManager
+import cstj.qc.ca.andromia.fragments.AccueilFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
+                                            AccueilFragment.OnFragmentInteractionListener {
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +32,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.contentFrame,AccueilFragment.newInstance())
+        transaction.commit()
     }
 
     override fun onBackPressed() {
