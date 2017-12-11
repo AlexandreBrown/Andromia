2cff284947aca598befd5bf745fa6b771648d122
AlexandreBrown <brownthedev@gmail.com>
2017-12-10 21:31:06 -0500

    vue creationCompte

--- app/src/main/java/cstj/qc/ca/andromia/activities/ConnexionActivity.kt
+++ app/src/main/java/cstj/qc/ca/andromia/activities/ConnexionActivity.kt
@@ -1,12 +1,11 @@
 package cstj.qc.ca.andromia.activities
 
-import android.app.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.WindowManager
 import cstj.qc.ca.andromia.R
 
-class ConnexionActivity : Activity(){
+class ConnexionActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/cstj/qc/ca/andromia/activities/CreationCompteActivity.kt
@@ -0,0 +1,13 @@
+package cstj.qc.ca.andromia.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import cstj.qc.ca.andromia.R
+
+class CreationCompteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_creation_compte)
+    }
+}
