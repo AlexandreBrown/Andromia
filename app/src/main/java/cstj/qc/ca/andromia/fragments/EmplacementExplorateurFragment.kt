dac85f6c2169e3f0fecc0641b55cd11a45dabdd8
AlexandreBrown <brownthedev@gmail.com>
2017-12-19 19:33:06 -0500

    ajout des positions en px

--- app/src/main/java/cstj/qc/ca/andromia/fragments/EmplacementExplorateurFragment.kt
+++ app/src/main/java/cstj/qc/ca/andromia/fragments/EmplacementExplorateurFragment.kt
@@ -49,18 +49,86 @@ class EmplacementExplorateurFragment : Fragment() {
         emplacement_explorateur_btn_affficher_runes.setOnClickListener {
             mListener!!.onShowMyRunesClick()
         }
+        updateLocation()
 
-        drawMap(405,167)
     }
 
-    fun drawMap(xPost:Int,yPos:Int){
+    fun updateLocation(){
+        val emplacement = "yartar"
+        if(!emplacement.isBlank()){
+            when (emplacement){
+                "mordukin" ->{
+                    drawCurrentLocation(95,90)
+                }
+                "yartar"->{
+                    drawCurrentLocation(285,50)
+                }
+                "everlund"->{
+                    drawCurrentLocation(195,240)
+                }
+                "atomico"->{
+                    drawCurrentLocation(405,167)
+                }
+                "bézantur"->{
+                    drawCurrentLocation(737,66)
+                }
+                "trois-épées"->{
+                    drawCurrentLocation(340,440)
+                }
+                "inoxis"->{
+                    drawCurrentLocation(557,395)
+                }
+                "lunargent"->{
+                    drawCurrentLocation(260,540)
+                }
+                "star nation"->{
+                    drawCurrentLocation(230,375)
+                }
+                "indigo"->{
+                    drawCurrentLocation(40,310)
+                }
+                "ilm garde"->{
+                    drawCurrentLocation(265,705)
+                }
+                "cordisphère"->{
+                    drawCurrentLocation(630,535)
+                }
+                "bois d'elm"->{
+                    drawCurrentLocation(760,480)
+                }
+                "myth dranon"->{
+                    drawCurrentLocation(768,200)
+                }
+                "apollo"->{
+                    drawCurrentLocation(915,190)
+                }
+                "rafalo land"->{
+                    drawCurrentLocation(985,100)
+                }
+                "deux-étoiles"->{
+                    drawCurrentLocation(985,340)
+                }
+                "bourok torn"->{
+                    drawCurrentLocation(725,762)
+                }
+                "volcano"->{
+                    drawCurrentLocation(850,750)
+                }
+                "n'jast"->{
+                    drawCurrentLocation(970,605)
+                }
+            }
+        }
+    }
+
+    fun drawCurrentLocation(xPost:Int,yPos:Int){
         val width = Resources.getSystem().displayMetrics.widthPixels
         val height = Resources.getSystem().displayMetrics.heightPixels
         var bitmap = Bitmap.createBitmap(width, height/2,Bitmap.Config.ARGB_8888)
         var cnv = Canvas(bitmap)
         var paint = Paint(Paint.ANTI_ALIAS_FLAG)
         paint.color = Color.RED
-        paint.alpha = 123
+        paint.alpha = 200
         val radius = 8
         val density = resources.displayMetrics.density
         cnv.drawCircle(
